dab67d0a287dddac33af2a4351f92dc29f9fc951
Michal Hrusecky <MichalHrusecky@Eaton.com>
2015-10-26 13:45:07 +0100

    Support multiple alerts

--- lua/lua.cc
+++ lua/lua.cc
@@ -10,6 +10,7 @@ extern "C" {
 #include <iostream>
 #include <fstream>
 #include <cxxtools/jsondeserializer.h>
+#include <cxxtools/directory.h>
 
 #include "agents.h"
 #include "bios_agent.h"
@@ -22,27 +23,40 @@ struct config {
   std::string out;
 };
 
+std::map<std::string, std::vector<config>> configs;
+
 int main (int argc, char** argv) {
   char buff[256];
   int error;
-  config cfg;
 
   bios_agent_t *client = bios_agent_new("ipc://@/malamute", argv[0]);
 
   // Read configuration
-  std::ifstream f("conf.json");
-  cxxtools::JsonDeserializer json(f);
-  json.deserialize();
-  const cxxtools::SerializationInfo *si = json.si();
-  si->getMember("evaluation") >>= cfg.lua_code;
-  si->getMember("in") >>= cfg.in;
-  si->getMember("out") >>= cfg.out;
+  cxxtools::Directory d(".");
+  for(auto fn : d) {
+    config cfg;
+    if(fn.length() < 5) continue;
+    if(fn.compare(fn.length() - 5, 5, ".json") !=0 )
+      continue;
+    std::ifstream f(fn);
+    cxxtools::JsonDeserializer json(f);
+    json.deserialize();
+    const cxxtools::SerializationInfo *si = json.si();
+    si->getMember("evaluation") >>= cfg.lua_code;
+    si->getMember("in") >>= cfg.in;
+    si->getMember("out") >>= cfg.out;
 
-  // Subscribe to all streams
-  for(auto it : cfg.in) {
-    bios_agent_set_consumer(client, bios_get_stream_measurements(), it.c_str());
-    printf("Registered to receive '%s'\n", it.c_str());
+    // Subscribe to all streams & store configuration
+    for(auto it : cfg.in) {
+      bios_agent_set_consumer(client, bios_get_stream_measurements(), it.c_str());
+      printf("Registered to receive '%s'\n", it.c_str());
+      if(configs.find(it.c_str()) == configs.end())
+        configs.insert(std::make_pair(it.c_str(), std::vector<config>({})));
+      auto cit = configs.find(it.c_str());
+      cit->second.push_back(cfg);
+    }
   }
+
 
   while(!zsys_interrupted) {
     ymsg_t *yn = bios_agent_recv(client);
@@ -62,29 +76,34 @@ int main (int argc, char** argv) {
     }
     ymsg_destroy(&yn);
 
-    // Do we have everything?
-    if(cache.size() != cfg.in.size())
-      continue;
+    auto top = configs.find(topic);
+    for(auto cfg : configs.find(topic)->second) {
+      // Compute
+      lua_State *L = lua_open();
+      for(auto i : cfg.in) {
+        auto it = cache.find(i);
+        if(it == cache.end()) {
+          printf("Do not have everything for '%s' yet\n", cfg.out.c_str());
+          goto next_cfg;
+        }
+        std::string var = it->first;
+        var[var.find('@')] = '_';
+        printf("Setting variable '%s' to %lf\n", var.c_str(), it->second);
+        lua_pushnumber(L, it->second);
+        lua_setglobal(L, var.c_str());
+      }
+      error = luaL_loadbuffer(L, cfg.lua_code.c_str(), cfg.lua_code.length(), "line") ||
+              lua_pcall(L, 0, 2, 0);
 
-    // Compute
-    lua_State *L = lua_open();
-    for(auto it : cache) {
-      std::string var = it.first;
-      var[var.find('@')] = '_';
-      printf("Setting variable '%s' to %lf\n", var.c_str(), it.second);
-      lua_pushnumber(L, it.second);
-      lua_setglobal(L, var.c_str());
+      if(lua_isnumber(L, -1))
+          fprintf(stdout, "ALERT (%s = %lf), %s\n", cfg.out.c_str(), lua_tonumber(L, -1), lua_tostring(L, -2));
+      if (error) {
+        fprintf(stderr, "%s", lua_tostring(L, -1));
+        lua_pop(L, 1);  /* pop error message from the stack */
+      }
+next_cfg:
+      lua_close(L);
     }
-    error = luaL_loadbuffer(L, cfg.lua_code.c_str(), cfg.lua_code.length(), "line") ||
-            lua_pcall(L, 0, 2, 0);
-
-    if(lua_isnumber(L, -1))
-        fprintf(stdout, "ALERT (%s = %lf), %s\n", cfg.out.c_str(), lua_tonumber(L, -1), lua_tostring(L, -2));
-    if (error) {
-      fprintf(stderr, "%s", lua_tostring(L, -1));
-      lua_pop(L, 1);  /* pop error message from the stack */
-    }
-    lua_close(L);
   }
   bios_agent_destroy(&client);
   return 0;
