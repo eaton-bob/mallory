3101a4cbef886fbe9924d7557cee659434fe3f9f
Michal Vyskocil <michalvyskocil@eaton.com>
2015-10-26 11:34:34 +0100

    Problem: need to receive emails
    Solution: add it

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 gcc -std=c11 -ggdb -D_SVID_SOURCE -lmlm -lczmq -lzmq src/alerts.c -o alerts
+gcc -std=c11 -ggdb -lmlm -lczmq -lzmq src/email.c -o email
 g++ -std=c++11 -ggdb -lmlm -lczmq -lzmq src/ui.cc -o ui
